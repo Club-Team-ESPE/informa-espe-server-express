145d49570c1ec6ba54e2277bf82194d5d9f24d31
Sammy593 <samuelto1359@gmail.com>
2024-07-12 23:12:54 -0500

    CORS options

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
 import express from 'express' // ESModules
 // Middlewares
+import cors from 'cors'
+import corsOptions from './config/corsOptions'
 import { errorHandler } from './middlewares/errorHandler'
+// Tools
+import moment from 'moment-timezone'
 // Routes
 import mobileApiRoutes from './routes/mobil/index'
 
-import moment from 'moment-timezone'
-
 const app = express()
-
-process.env.TZ = 'America/Guayaquil'
-
-const date = new Date()
-const localDate = moment(date).tz('America/Guayaquil').format()
-
-console.log('Fecha en zona horaria local:', localDate)
 
 // Middlewares
 app.use(express.json())
+app.use(cors(corsOptions))
 
 // Routes
 app.use('/api/mobil', mobileApiRoutes)
-/*
+
 app.all('*', (_req, res) => {
   res.status(404)
   res.json({ error: '404 Not Found' })
-}) */
+})
+
 app.use(errorHandler)
+
+process.env.TZ = 'America/Guayaquil'
+const date = new Date()
+const localDate = moment(date).tz('America/Guayaquil').format()
+console.log('Fecha en zona horaria local:', localDate)
 
 export { app }
---
+++ src/config/corsOptions.ts
@@ -0,0 +1,7 @@
+const allowedOrigins = ['http://localhost:8100']
+
+const corsOptions = {
+  origin: allowedOrigins
+}
+
+export default corsOptions
